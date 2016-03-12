dbc8b9417d25dd72ff3785b3ab8c9cdf749fc97f
Rich Song <richasong@gmail.com>
2016-03-12 16:14:52 -0500

    Restructuring and Refactoring again to address potential issues

--- browser/js/game/game.js
+++ browser/js/game/game.js
@@ -2,7 +2,6 @@
 
 function Game () {}
 
-var game = new Phaser.Game(512,512, Phaser.AUTO, 'game');
 
 Game.prototype.preload = function () {
     this.load.image('black', '../../assets/black.png');
@@ -39,6 +38,9 @@ Game.prototype.create = function () {
                 var idx = x * 5 + y;
                 tileSprites[idx].x = x * TILE_SIZE;
                 tileSprites[idx].y = y * TILE_SIZE;
+                tileSprites[idx].inputEnabled = true;
+                tileSprites[idx].input.draggable= true;
+                //tileSprites[idx].event.onDragStart(console.log('hi'))
                 tileRow.push(tileSprites[idx]);
             }
             tiles.push(tileRow);
@@ -49,13 +51,15 @@ Game.prototype.create = function () {
     };
 
     this.tiles = this.tileSprites();
-    var test = this.tiles[4][4];
-    console.log(test.key)
-    console.log(test.x)
-    console.log(test.y)
+    var test = this.tiles[0][0];
+    console.log(test);
+
+
 };
 
 Game.prototype.update = function () {
+    // if this is selected, console.log it's position right now
+    // sprite.input.pointerData => id, isDown, isDragged, isOut, isOver, isUp
 
 };
 
@@ -78,14 +82,58 @@ Game.prototype.shuffle = function (array) {
     return array;
 };
 
-
+var game = new Phaser.Game(512,512, Phaser.AUTO, 'game');
 game.state.add('game', Game);
 game.state.start('game');
 
 
 
-
-
+//
+//
+//TestGame.Unit = function (game, x, y, faction, job) {
+//    var frameIdx = faction * TestGame.jobs.length + job;
+//    Phaser.Sprite.call(this, game, x, y, 'spritesheet-units', frameIdx);
+//    this.name = 'unit';
+//
+//    /* INITIALIZATION */
+//
+//    this.inputEnabled = true;
+//    this.input.useHandCursor = true;
+//    this.events.onInputDown.add(this.select, this);
+//    this.events.onInputUp.add(this.release, this);
+//    return this;
+//};
+//
+//
+//TestGame.Unit.prototype = Object.create(Phaser.Sprite.prototype);
+//TestGame.Unit.prototype.constructor = TestGame.Unit;
+//TestGame.Unit.prototype.select = function () {
+//    this.game.events.onUnitSelected.dispatch(this);
+//};
+//
+//TestGame.Unit.prototype.selectMove = function () {
+//    this.game.events.onUnitMoveSelect.dispatch(this);
+//    this.game.input.onUp.add(this.processClickMove, this);
+//};
+//TestGame.Unit.prototype.processClickMove = function (pointer) {
+//    if (pointer.duration <= 150) {
+//        // in case they are dragging
+//        // this.moveTo({x: pointer.worldX, y:pointer.worldY});
+//        // this.game.input.onUp.remove(this.processClickMove, this);
+//        // }};
+//        TestGame.Game.prototype = {
+//            create: function () {
+//                if (!this.game.events) this.game.events = {};
+//                this.game.events.onUnitSelected = new Phaser.Signal();
+//                this.game.events.onUnitMoveSelect = new Phaser.Signal();
+//                this.game.events.onUnitSelected.add(this.handleUnitSelect, this);
+//                this.game.events.onUnitMoveSelect.add(this.handleUnitMoveSelect, this);
+//            }, update: function () {        ...
+//            }...spawnUnit
+//    :
+//        function (castle) {
+//            var unit;		// game, x, y, faction, icon		unit = new TestGame.Unit(this.game, 			    castle.x + 16,			    castle.y + 16,			    castle.properties.faction,			    this.game.rnd.integerInRange(0, TestGame.jobs.length-1)			);		this.game.add.existing(unit);		party.revive(100);		if (TestGame.factions[unit.properties.faction] === 'player') {		    this.playerParties.add(unit);		} else if (unit.properties.faction === 'foe') {		    this.foeParties.add(unit);		} else {		    this.neutralParties.add(unit);		}	},		handleUnitSelect : function (unit) {		if (TestGame.factions[unit.properties.faction] === 'player') {		    this.playerUnitSelected = unit;		    this.selectedUnitMenu.clearButtonHandle('move');		    this.selectedUnitMenu.clearButtonHandle('cancel');		    		    this.selectedUnitMenu.addButtonHandle('move', unit.selectMove, unit);		    this.selectedUnitMenu.addButtonHandle('cancel', this.selectedUnitMenu.hide, this.selectedUnitMenu);		    		    this.selectedPartyMenu.show({			x : party.x + 64,			y : party.y		    });		}	},	handleUnitMoveSelect : function (unit) {		this.selectedUnitMenu.hide();	}};
+//
 
 
 
