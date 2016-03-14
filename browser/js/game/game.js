2002f5a6091ce0abc17d7b35acf0e5964a478cae
Rich Song <richasong@gmail.com>
2016-03-14 01:56:28 -0400

    Major issue with the general structure of the game

--- browser/js/game/game.js
+++ browser/js/game/game.js
@@ -2,7 +2,9 @@
 
 function Game () {}
 
+
 Game.prototype.preload = function () {
+
     this.load.image('black', '../../assets/black.png');
     this.load.image('white', '../../assets/white.png');
     this.load.image('red', '../../assets/red.png');
@@ -16,8 +18,9 @@ Game.prototype.create = function () {
 
     this.physics.startSystem(Phaser.Physics.ARCADE);
 
+    // TILE SPRITES //// TILE SPRITES //// TILE SPRITES //// TILE SPRITES //
     this.tileSprites = function () {
-        var TILE_SIZE = 65,
+        var TILE_SIZE = 32.5,
             tileSprites = [],
             tiles = [],
             imageKey = ['white','red','blue', 'orange', 'green', 'yellow'];
@@ -50,7 +53,7 @@ Game.prototype.create = function () {
                 t.y = y * TILE_SIZE;
                 t.inputEnabled = true;
                 t.input.draggable = true;
-                t.hitArea = new Phaser.Rectangle(32,32,65,65);
+                t.hitArea = new Phaser.Rectangle(16,16,32,32);
 
                 t.input.snapOnRelease = true;
                 t.input.snapX = TILE_SIZE;
@@ -66,11 +69,65 @@ Game.prototype.create = function () {
     };
 
     this.tiles = this.tileSprites();
+    // TILE SPRITES //// TILE SPRITES //// TILE SPRITES //
+
+
+
+
+    // WINNING PATTERN //// WINNING PATTERN //// WINNING PATTERN //
+    this.winningPattern = function () {
+        var patternSprites = [],
+            patternKey = ['white','red','blue', 'orange', 'green', 'yellow','white','red','blue', 'orange', 'green', 'yellow'];
+
+
+        var patternIdx1 = Math.floor(Math.random() * 10  );
+        var patternIdx2 = Math.floor(Math.random() * 10  );
+        var patternIdx3 = Math.floor(Math.random() * 10  );
+        var patternIdx4 = Math.floor(Math.random() * 10  );
+        var patternIdx5 = Math.floor(Math.random() * 10  );
+        var patternIdx6 = Math.floor(Math.random() * 10  );
+        var patternIdx8 = Math.floor(Math.random() * 10  );
+        var patternIdx9 = Math.floor(Math.random() * 10  );
+
+
+        patternSprites.push([this.add.sprite(0, 320, patternKey[patternIdx1]), this.add.sprite(32.5, 320, patternKey[patternIdx4]), this.add.sprite(65, 320, patternKey[patternIdx5])])
+        patternSprites.push([this.add.sprite(32.5, 352, patternKey[patternIdx2]), this.add.sprite(65, 352, patternKey[patternIdx6]), this.add.sprite(0, 352, patternKey[patternIdx8])]);
+        patternSprites.push([this.add.sprite(0, 384, patternKey[patternIdx6]), this.add.sprite(32.5, 384, patternKey[patternIdx3]), this.add.sprite(65, 384, patternKey[patternIdx9])]);
+
+
+        return patternSprites;
+    };
+
+    this.pattern = this.winningPattern();
+    //console.log(Object.keys(this.pattern[0][0]));
+    // WINNING PATTERN //// WINNING PATTERN //// WINNING PATTERN //
+
+
 
 
 };
 
 Game.prototype.update = function () {
+
+    this.insideGrid = [
+        [this.tiles[1][1], this.tiles[1][2], this.tiles[1][3] ],
+        [this.tiles[2][1], this.tiles[2][2], this.tiles[2][3] ],
+        [this.tiles[3][1], this.tiles[3][2], this.tiles[3][3] ]
+    ];
+
+    if ( this.pattern[0][0].key === this.insideGrid[0][0].key &&
+         this.pattern[0][1].key === this.insideGrid[0][1].key &&
+         this.pattern[0][2].key === this.insideGrid[0][2].key &&
+         this.pattern[0][0].key === this.insideGrid[0][0].key &&
+         this.pattern[0][1].key === this.insideGrid[0][1].key &&
+         this.pattern[0][2].key === this.insideGrid[0][2].key &&
+         this.pattern[0][0].key === this.insideGrid[0][0].key &&
+         this.pattern[0][1].key === this.insideGrid[0][1].key &&
+         this.pattern[0][2].key === this.insideGrid[0][2].key
+    ) { console.log("WIN") }
+
+
+
     // if this is selected, console.log it's position right now
     // sprite.input.pointerData => id, isDown, isDragged, isOut, isOver, isUp
 
@@ -133,7 +190,7 @@ Game.prototype.shuffle = function (array) {
     return array;
 };
 
-var game = new Phaser.Game(512,512, Phaser.CANVAS, 'game');
+var game = new Phaser.Game(320,525, Phaser.CANVAS, 'game');
 game.state.add('game', Game);
 game.state.start('game');
 
@@ -146,30 +203,6 @@ game.state.start('game');
 
 
 
-/*
- var Board = function (columns, rows) {
-
- var board = [];
- var group = game.add.group();
-
- for (var y=0; y<rows; y++) {
- var row = [];
-
- for (var x=0; x<columns; x++) {
- var tile = new Tile(x, y, group);
- row.push(tile);
- }
-
- board.push(row);
- }
-
- this.moveTo = function (x, y) {
- group.x = x;
- group.y = y;
- };
-
- };
- */
 
 
 
